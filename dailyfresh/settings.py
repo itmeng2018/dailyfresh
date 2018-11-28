e3a97e28a56a446ce8914c922b3d4a2235ae7b0f
itmeng <yanmengfei@mac.local>
2018-11-28 21:31:56 +0800

    搜索页和详情页实现

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tinymce',  # 富文本编辑器
+    'haystack',  # 全文检索框架
     'user',  # 用户模块
     'cart',  # 购物车模块
     'order',  # 订单模块
@@ -161,3 +162,20 @@ FDFS_URL = 'http://192.168.154.129:8888/'
 
 # 调用client.conf文件的路径
 FDFS_CLIENT_CONF = './utils/fdfs/client.conf'
+
+# 全文检索框架的配置
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        # 使用whoosh引擎
+        # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+        # 索引文件路径
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
+    }
+}
+
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+# 指定搜索结果每页显示的条数
+HAYSTACK_SEARCH_RESULTS_PER_PAGE=1
